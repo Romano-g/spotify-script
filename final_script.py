807504b1a9339402b531721a6a4e7daa296305db
Caio Romano <caioromano13@gmail.com>
2023-12-12 02:08:42 -0300

    12.12.23-organized

--- final_script.py
+++ final_script.py
@@ -16,19 +16,21 @@ artists_file = open('artists.txt', 'r')
 artists = [x.strip('\n') for x in artists_file.readlines()]
 artists_number = len(artists)
 
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id= client_id_env,
-                                               client_secret= client_secret_env,
-                                               redirect_uri= client_redirect_uri_env,
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id_env,
+                                               client_secret=client_secret_env,
+                                               redirect_uri=client_redirect_uri_env,
                                                scope="user-library-read"))
 
 results_playlist = sp.playlist_items(playlist_id_env)
 results_ids = []
+
 
 def checker(offset: int):
     result_playlist_musics = sp.playlist_items(playlist_id_env, offset=offset)
     for id in range(len(result_playlist_musics['items'])):
         results_ids.append(result_playlist_musics['items'][id]['track']['id'])
     return
+
 
 checker(0)
 checker(100)
@@ -44,12 +46,12 @@ for x in range(0, artists_number):
     id_path = result['tracks']['items'][0]['artists'][0]['id']
 
     ids.append(id_path)
-    
+
 music_ids = []
 
 for id in ids:
     artista = sp.artist_top_tracks(id)
-        
+
     try:
         for x in range(5):
             music_id = artista['tracks'][x]['id']
@@ -63,13 +65,16 @@ for id in ids:
     except IndexError:
         continue
 
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id= client_id_env,
-                                               client_secret= client_secret_env,
-                                               redirect_uri= client_redirect_uri_env,
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id_env,
+                                               client_secret=client_secret_env,
+                                               redirect_uri=client_redirect_uri_env,
                                                scope="playlist-modify-public"))
 
+
 def add_track(start: int, end: int):
-    sp.user_playlist_add_tracks(username_env, playlist_id_env, music_ids[start:end])
+    sp.user_playlist_add_tracks(
+        username_env, playlist_id_env, music_ids[start:end])
+
 
 try:
     add_track(0, 100)
